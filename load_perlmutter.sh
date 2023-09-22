29c79f790f9b1092bb4e1cb55142dfcd4a6fcd96
physnerds <amitbashyal@gmail.com>
2023-09-22 08:08:17 -0700

    Replace nightlies version of json with a more stable one

--- load_perlmutter.sh
+++ load_perlmutter.sh
@@ -7,8 +7,10 @@ module load cmake
 module load eigen
 
 export VDT_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_102/vdt/0.4.3/x86_64-centos8-gcc11-opt
-export JSON_CMAKE=/cvmfs/atlas-nightlies.cern.ch/repo/sw/master_Athena_x86_64-centos7-gcc11-opt/sw/lcg/releases/LCG_102b_ATLAS_13/jsonmcpp/3.10.5/x86_64-centos7-gcc11-opt/lib64/cmake
+#export JSON_CMAKE=/cvmfs/atlas-nightlies.cern.ch/repo/sw/master_Athena_x86_64-centos7-gcc11-opt/sw/lcg/releases/LCG_102b_ATLAS_13/jsonmcpp/3.10.5/x86_64-centos7-gcc11-opt/lib64/cmake
+export JSON_CMAKE=/cvmfs/sft.cern.ch/lcg/releases/LCG_102/jsonmcpp/3.10.5/x86_64-centos9-gcc11-opt/lib64/cmake
 export BOOST_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_102/Boost/1.78.0/x86_64-centos9-gcc11-opt
 export M_EIGEN_DIR=$EIGEN_DIR/include/eigen3
 
 alias serialcmake='cmake ..  -DCMAKE_PREFIX_PATH=$JSON_CMAKE'
+alias makeclean='make clean; rm -rf CMakeFiles; rm -rf CMakeCache.txt; rm -rf G__*; rm Makefile'
