668b476d4f8916b10bd98f4870594ad350208a2f
physnerds <amitbashyal@gmail.com>
2023-09-27 07:53:44 -0700

    Create Branch for the SoA instead

--- test_tree.cxx
+++ test_tree.cxx
@@ -18,40 +18,60 @@ int readTree(){
   DuneSoADataModel::soa_arrays* Soa;
   
   auto _rfile = new TFile(_fname,"READONLY");
+  TTree *fTree = _rfile->Get<TTree>("myTree");
   
-  Soa = (DuneSoADataModel::soa_arrays*)_rfile->Get("mySoa");
+  Soa = new DuneSoADataModel::soa_arrays;
+  fTree->SetBranchAddress("Soa",&Soa);
+  const int entries = fTree->GetEntries();
+  std::cout<<"Total Entries "<<entries<<std::endl;
+
+  fTree->GetEntry(0);
   
   for(int i = 0;i<10;i++)
     std::cout<<Soa->xx[i]<<" "<<Soa->yy[i]<<" "<<Soa->zz[i]<<std::endl; 
   
   delete Soa;
+  
   return 1;
 }
 
 int createTree(){
+   TFile *fOutFile;
+  TTree* fOutTree;
+  DuneSoADataModel::soa_arrays* Soa;
+  Soa = new DuneSoADataModel::soa_arrays;
+  constexpr char* const _fname = "myfile_tree.root";
+  
+  fOutFile = new TFile(_fname,"RECREATE");
+  fOutTree = new TTree("myTree","Test of SoA in Tree");
+  fOutTree->Branch("Soa","DuneSoADataModel::soa_arrays",&Soa, 32000,99);
+  
 
-    DuneSoADataModel::soa_arrays Soa;
   
   for(int i=0;i<10000;i++){
     
-    Soa.xx[i] = i;
-    Soa.yy[i] = (i)*3.1415;
-    Soa.zz[i] = i+8;
+    Soa->xx[i] = i;
+    Soa->yy[i] = (i)*3.1415;
+    Soa->zz[i] = i+8;
   }
-  constexpr char* const _fname = "myfile_tree.root";
+    Soa->_size = 10000;
+
   
   for(int i = 0;i<10;i++)
-    std::cout<<Soa.xx[i]<<" "<<Soa.yy[i]<<" "<<Soa.zz[i]<<std::endl;
+    std::cout<<Soa->xx[i]<<" "<<Soa->yy[i]<<" "<<Soa->zz[i]<<std::endl;
   
-  TFile* _rfile = new TFile(_fname,"RECREATE");
-  _rfile->WriteObjectAny(&Soa,"DuneSoADataModel::soa_arrays","mySoa");
+ // fOutFile->WriteObjectAny(Soa,"DuneSoADataModel::soa_arrays","mySoa");
+  fOutTree->Fill();
   
-  delete _rfile;
+  fOutTree->Write();
+  fOutFile->Close();
+  delete Soa;
+  delete fOutFile;
   return 1;
 }
 
 int main(){
- // createTree();
+// createTree();
   readTree();
   return EXIT_SUCCESS;
 }
